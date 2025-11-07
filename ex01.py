681542be2ab8867cd358aa39fa7b69d7dea28f73
Sne <sweetsonu2006in@gmail.com>
2025-11-07 18:56:51 +0530

    Initialize variables k, PI, and name

---
+++ ex01.py
@@ -0,0 +1,12 @@
+# Write code that initializes the following variables:
+#k with the value 88
+#PI with the value 3.14
+#name with the value "bob"
+k = 88;
+PI = 3.14;
+name = "bob";
+
+# Don't change the line below
+print(f"k = {k}")
+print(f"PI = {PI}")
+print(f"name = {name}")
