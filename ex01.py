e2303ad5ee5ff215b86e6c80cf04d588c866bd79
Sne <sweetsonu2006in@gmail.com>
2025-11-07 19:53:09 +0530

    Update ex01.py

--- ex01.py
+++ ex01.py
@@ -2,9 +2,9 @@
 #k with the value 88
 #PI with the value 3.14
 #name with the value "bob"
-k = 88;
-PI = 3.14;
-name = "bob";
+k = 88
+PI = 3.14
+name = "bob"
 
 # Don't change the line below
 print(f"k = {k}")
