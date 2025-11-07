ea16ffa7ad3be01c956e1d3ab14e59aafeb0d5b4
Sne <sweetsonu2006in@gmail.com>
2025-11-07 20:04:06 +0530

    Implement basic arithmetic and logical operations

---
+++ ex02.py
@@ -0,0 +1,21 @@
+# Add 4 to count
+#Multiply count by 2
+#Subtract 1 from count
+count = 0
+count += 4
+count *= 2
+count -= 1
+print(f"count = {count}")
+
+# Write a script that initializes 2 variables n1 and n2 with the values 8 and 9 (accordingly).
+#After that initialize another variable n3 that will hold whether n1 is bigger than n2.
+n1 = 8
+n2 = 9
+n3 = n1 > n2
+print(f"n1 = {n1}, n2 = {n2}, n3 = {n3}")
+
+# Replace the question marks of the variables b1 and b2 so that b3 holds True.
+b1 = (7> 10 or 2<5) 
+b2 = not 1==1
+b3 = b1 or b2
+print(f"b3 = {b3}")
