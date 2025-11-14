272c0fd1bbcae9eb0d2268d2171ec87f5118df9d
Sne <sweetsonu2006in@gmail.com>
2025-11-14 18:50:42 +0530

    Update ex08.py

--- ex08.py
+++ ex08.py
@@ -24,3 +24,25 @@ val1 = int(input())
 val2 = int(input())
 addNum(val1,val2)
 
+#Create a function that receives two arguments and returns the bigger number of the two. 
+#if both are equal then return one of them.
+iterations = int(input())
+num1 = int(input())
+num2 = int(input())
+
+def bigger(arg1, arg2):
+    if arg1 > arg2:
+        return arg1
+    else:
+        return arg2
+
+for i in range(iterations):
+    if num1 < 2 or num2 <2:
+        break
+    result = bigger(num1,num2)
+    if result == num1:
+        num1 /= 2
+        print(num1)
+    elif result == num2:
+        num2 /= 2
+        print(num2)
