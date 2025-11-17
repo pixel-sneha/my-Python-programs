c2cb77505b941d9e037f6eccf034f668441fffeb
Sne <sweetsonu2006in@gmail.com>
2025-11-17 20:46:01 +0530

    Update ex09.py

--- ex09.py
+++ ex09.py
@@ -17,3 +17,17 @@ for i in range(totalInput):
     if i == totalInput:
         break
 print(dynamicNum)
+
+#Write a function named is_valid that gets two strings arguments, username and password.
+#The function will return True if the username and password are valid in the system, otherwise Fals
+def is_valid(username, password):
+    if username=="user":
+        if password=="qweasd":
+            return True
+        else:
+            return False
+    elif username=="admin":
+        return True
+
+    else:
+        return False
