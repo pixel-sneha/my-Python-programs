2c3954fd35f6fc62a13ee83d5d7f579c0fd3d3b2
Sne <sweetsonu2006in@gmail.com>
2025-11-27 22:49:44 +0530

    Update ex10.py

--- ex10.py
+++ ex10.py
@@ -4,3 +4,28 @@ def reverse(lst):
     for i in range(len(lst) -1,-1,-1):
         revlst.append(lst[i])
     return(revlst)
+
+Each test case has one input - an odd whole number.
+Your task is to print n - pyramid using * 
+n = int(input())
+for stars in range(1, n + 1, 2):
+    if n<=0 or n%2==0:
+        break
+    else:
+        print("*" * stars)
+
+#Create a function names transpose that receives one argument:
+# A list that contains lists. and returns a modified list.
+def transpose(lst):
+    if len(lst)==0:
+        return lst
+    modified_list = []
+    row_length = len(lst[0])
+    col_length = len(lst)
+    for i in range(row_length):
+        updated_rows = []
+        for j in range(col_length):
+            updated_rows.append(lst[j][i])
+        modified_list.append(updated_rows)
+    return modified_list
+      
