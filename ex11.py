1bb3660dd6c8e0b61dbc3e08c5b681466cc85019
Sne <sweetsonu2006in@gmail.com>
2025-11-28 09:54:49 +0530

    Add whatToBuy function for construction item selection
    Implement whatToBuy function to calculate affordable construction items.

---
+++ ex11.py
@@ -0,0 +1,25 @@
+#Create a function named whatToBuy that receives four arguments:
+#First argument is a list of numbers that indicate the price of a construction item
+#Second argument is a list of numbers that indicates the quantity of each construction item
+#Third argument is a number indicating the total price allowed for each item
+#Fourth argument is a list of construction names
+#The function calculates, for each item, the total price required to buy it. return sorted list of names satisfying the condtion and number of items removed
+def whatToBuy(prices, quantities, restriction, names):
+    if len(prices)==0:
+        return [[], 0]
+    average_prices = sum(prices)/len(prices)
+    average_quantities = sum(quantities)/ len(quantities)
+    new_list = []
+    total_names = 0
+    for i in range(len(prices)):
+        price = prices[i]
+        quantity = quantities[i]
+        name = names[i]
+        total_price = (quantity * price) / (average_quantities * average_prices)
+        if total_price < restriction:
+            new_list.append(name)
+        else:
+            total_names += 1
+    new_list.sort()
+    return [new_list, total_names]
+    
