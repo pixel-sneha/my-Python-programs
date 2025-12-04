96a4a234c30146b9ef64a6d2b64c7b90db5f6a7c
Sne <sweetsonu2006in@gmail.com>
2025-12-04 16:56:00 +0530

    Update ex12.py

--- ex12.py
+++ ex12.py
@@ -4,3 +4,12 @@ if len(input_list) >= 5:
 else:
     print([input_list[0], input_list[len(input_list) - 1]])
 
+#Create a program that receives a list as input (given) and prints three new lists based on the following slicing operations:
+#A list containing every third element, starting from index 1 (the second element)
+#A list containing all the elements from the 6th element to the 1st in reverse order
+#A list containing every second element starting from the middle of the list to the end
+lst = input().split(",")
+size = len(lst)//2
+print(lst[1::3])
+print(lst[5::-1])
+print(lst[size::2])
