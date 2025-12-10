56971a42c257a14332d7db68a4daf6d0a61d21a6
Sne <sweetsonu2006in@gmail.com>
2025-12-10 12:54:43 +0530

    Update ex12.py

--- ex12.py
+++ ex12.py
@@ -50,3 +50,17 @@ color1, color2, color3 = colors
 print(f"Name: {name}, Age: {age}, City: {city}")
 print(f"x: {x}, y: {y}, z: {z}")
 print(f"Colors: {color1}, {color2}, {color3}")
+
+#Write a Python program that demonstrates the use of placeholder variables
+# Loop 5 times using a placeholder variable
+for _ in range(5):
+    print("Iteration")
+# List of numbers
+numbers = [10, 20, 30, 40, 50]
+
+# Unpack the list using placeholder variables
+first , _ , middle , _ , last = numbers
+# Print the values of first, middle, and last
+print(f"First: {first}")
+print(f"Middle: {middle}")
+print(f"Last: {last}")
