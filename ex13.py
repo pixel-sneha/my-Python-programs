0fec54212651954957c45b2c6e2cccd325a3317e
Sne <sweetsonu2006in@gmail.com>
2025-12-11 18:12:56 +0530

    Create ex13.py

---
+++ ex13.py
@@ -0,0 +1,7 @@
+# Write a program that:
+#Takes a number as input from the user (float).
+#takes the number of decimal places to round to (integer).
+#Outputs the rounded number.
+num = float(input())
+places = int(input())
+print(round(num,places))
