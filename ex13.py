7e83c84a03d94863474eb06da752fac6bf1d4621
Sne <sweetsonu2006in@gmail.com>
2025-12-13 22:03:07 +0530

    Update ex13.py

--- ex13.py
+++ ex13.py
@@ -37,3 +37,9 @@ def create_student_dict(name, age, major):
         "major": major
     }
     return dictionary
+
+#Create a function named get_capital that takes two parameters: country_capitals (a dictionary) and country_name (a string). The function should 
+#return the capital city of the given country name using the country_capitals dictionary.
+
+def get_capital(country_capitals, country_name):
+    return country_capitals[country_name]
