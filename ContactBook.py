c8723259e476d3556941383470767d2fe5240fd6
Sne <sweetsonu2006in@gmail.com>
2025-12-26 23:17:27 +0530

    Create ContactBook.py

---
+++ ContactBook.py
@@ -0,0 +1,9 @@
+def display_menu(): 
+    print("Contact Book Menu:")
+    print("1. Add Contact")
+    print("2. View Contact")
+    print("3. Edit Contact")
+    print("4. Delete Contact")
+    print("5. List All Contacts")
+    print("6. Exit")
+    return display_menu
