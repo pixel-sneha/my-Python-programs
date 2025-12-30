854359c1adf61b1c98de5b6094ea9622bfbefd50
Sne <sweetsonu2006in@gmail.com>
2025-12-30 22:34:11 +0530

    Create ex14.py

---
+++ ex14.py
@@ -0,0 +1,9 @@
+employees = {"Alice": "HR", "Bob": "Engineering", "Diana": "Marketing"}
+
+# You are managing a dictionary of employee data,
+# where each key is an employee's name and the value is their department.
+if "Alice" in employees:
+    print("Alice is in the company.")
+if "John" not in employees:
+    print("John is not in the company.")
+
