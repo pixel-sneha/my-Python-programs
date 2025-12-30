31319df3f181d2300f73917f24de2a334fbf0124
Sne <sweetsonu2006in@gmail.com>
2025-12-30 15:30:44 +0530

    Update ContactBook.py

--- ContactBook.py
+++ ContactBook.py
@@ -7,6 +7,31 @@ def display_menu():
     print("4. Delete Contact")
     print("5. List All Contacts")
     print("6. Exit")
-    return display_menu
 
+def add_contact(contact_book):
+    name = input()
+    if name in contact_book:
+        print("Contact already exists!")
+        return
+    phone = input()
+    email = input()
+    address = input()
+
+    contact_book[name] = {
+        "phone" : phone,
+        "email" : email,
+        "address" : address
+    }
+    print("Contact added successfully!")
+
+def view_contact(contact_book):
+    contact = input()
+    if contact in contact_book:
+        print(f"Name: {contact}")
+        print(f"Phone: {contact_book[contact]['phone']}")
+        print(f"Email: {contact_book[contact]['email']}")
+        print(f"Address: {contact_book[contact]['address']}")
+    else:
+        print("Contact not found!")
+        return
 
