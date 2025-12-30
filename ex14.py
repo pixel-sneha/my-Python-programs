b1d50e81cc344c25bd002b12115a9acdc088224c
Sne <sweetsonu2006in@gmail.com>
2025-12-30 22:48:31 +0530

    Update ex14.py

--- ex14.py
+++ ex14.py
@@ -7,3 +7,9 @@ if "Alice" in employees:
 if "John" not in employees:
     print("John is not in the company.")
 
+def check_inventory(inventory, item):
+    # Check if the item exists as a key in the inventory dictionary.
+    if item in inventory:
+        return f"{item} is in stock. Quantity: { inventory[item]}"
+    elif item not in inventory:
+        return f"{item} is not in stock."
