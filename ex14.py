017f83b2cc75db1d8c76297b5ad71ad48107cf09
Sne <sweetsonu2006in@gmail.com>
2026-01-02 20:12:48 +0530

    Update ex14.py

--- ex14.py
+++ ex14.py
@@ -13,3 +13,16 @@ def check_inventory(inventory, item):
         return f"{item} is in stock. Quantity: { inventory[item]}"
     elif item not in inventory:
         return f"{item} is not in stock."
+
+#Create a function named print_employee_details that takes a dictionary employee_data as an argument. 
+#The function should loop through the dictionary and print each key-value pair in the format 'key: value'.
+# If the dictionary is empty, the function should print 'No data available'
+def print_employee_details(employee_data):
+    if len(employee_data)==0:
+        print("No data available")
+        return
+    else:
+        for key, value in employee_data.items():
+            print(f'{key}: {value}')
+    #suppose you want capitalized key here, then use
+            print(f'{key.capitalize()}: {value}')
